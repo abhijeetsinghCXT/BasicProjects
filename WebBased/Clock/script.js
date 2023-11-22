8eb12b8239e633a41d1d4d10a66ce7f0aae83694
Abhijeet Singh <106686793+abhijeetsinghCXT@users.noreply.github.com>
2023-11-22 22:49:08 +0530

    Create script.js

---
+++ WebBased/Clock/script.js
@@ -0,0 +1,24 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const second = now.getSeconds();
+    const secondsDegrees = ((second/60)*360) + 90;
+    secondHand.style.transform= `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegree = ((mins/60)*360)+ ((second/60)*6) + 90;
+    minHand.style.transform= `rotate(${minsDegree}deg)`;
+
+    const hour = now.getHours();
+    const hourDegree = ((hour/12)*360)+ ((mins/60)*30) + 90;
+    hourHand.style.transform= `rotate(${hourDegree}deg)`;
+   
+} 
+
+setInterval(setDate, 1000);
+
+setDate();
